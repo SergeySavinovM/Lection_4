49ec8aa75d80c2ab8d0070d321b9384e282750d2
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-25 18:13:24 +0300

    добавил папку с началом лекции

---
+++ work_1/Program.cs
@@ -0,0 +1,60 @@
+﻿string [,] table = new string [2,5];
+//string.Empty
+//table[0,0]    table[0,1]  table[0,2]  table[0,3]  table[0,4]
+//table[1,0]    table[1,1]  table[1,2]  table[1,3]  table[1,4]
+
+// table[1,2] = "Слово";                                     
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         System.Console.WriteLine($"*{table[rows, columns]}*");
+//     }
+// }
+
+// int[,] matrix = new int [3, 4];
+
+// // for (int i = 0; i < 3; i++)
+// for (int i = 0; i < matrix.GetLength(0); i++)           //.GetLenght указывает на индекс переменной
+// {
+//     // for (int j = 0; j < 4; j++)
+//     for (int j = 0; j < matrix.GetLength(1); j++)       //.GetLenght указывает на индекс переменной
+//     {
+//         System.Console.Write($"{matrix[i, j]} ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+
+// void PrintArray(int [,] matr)
+// {
+// // for (int i = 0; i < 3; i++)
+// for (int i = 0; i < matr.GetLength(0); i++)         
+// {
+//     // for (int j = 0; j < 4; j++)
+//     for (int j = 0; j < matr.GetLength(1); j++)      
+//     {
+//         System.Console.Write($"{matr[i, j]} ");
+//     }
+//     System.Console.WriteLine();
+// }
+// }
+
+// void FillArray(int [,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)         
+// {
+//     for (int j = 0; j < matr.GetLength(1); j++)      
+//     {
+//         matr[i, j] = new Random().Next(1, 10);
+//     }
+// }
+// }
+
+// int[,] matrix = new int [3, 4];
+
+// PrintArray(matrix);
+// System.Console.WriteLine();
+// FillArray(matrix);
+// PrintArray(matrix);
